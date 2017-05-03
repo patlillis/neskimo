6f45a1993dd97cab818409729db7be5c277c7825
Pat Lillis <lillispm@gmail.com>
2017-05-02 21:15:11 -0600

    Bump version and start work for reading ROM.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+extern crate clap;
 #[macro_use]
 extern crate enum_primitive;
 extern crate num;
@@ -14,4 +15,23 @@ mod utils;
 #[cfg(test)]
 mod test;
 
-fn main() {}
+use clap::{Arg, App};
+
+// The version of neskimo that we're building.
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn main() {
+    let matches = App::new("neskimo")
+        .version(VERSION)
+        .author("Pat Lillis <lillispm@gmail.com>")
+        .about("A bare-bones NES emulator written in Rust.")
+        .arg(Arg::with_name("FILE")
+                 .help("The .nes ROM file to run")
+                 .required(true)
+                 .index(1))
+        .get_matches();
+
+    // .unwrap() is safe here FILE is required, so clap will crash if it's not there.
+    let file = matches.value_of("FILE").unwrap();
+    println!("Running ROM {}...", file);
+}
